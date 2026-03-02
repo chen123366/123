f5f407edc631eac98b602fa0ac01152693105a22
chen123366 <2545524300@qq.com>
2026-03-02 15:37:42 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -6,16 +6,21 @@ public class Login {
    public static void main(String[] args) {
        String rightUsername="admin";
        String rightPassword="123456";
-        System.out.println("登录系统");
-        System.out.println("请输入用户名");
         Scanner sc = new Scanner(System.in);
+      //有三次机会
+         for(int i=0;i<3;i++){
+              System.out.println("登录系统");
+        System.out.println("请输入用户名");
+      
         String username=sc.nextLine();
         System.out.println("请输入密码");
         String password=sc.nextLine();
         if(username.equals(rightUsername)&&password.equals(rightPassword)){
             System.out.println("登录成功");
+           break;
         }else{
             System.out.println("登录失败");
         }
-    }
+
 }
+
